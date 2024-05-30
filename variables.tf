a0fa85684d4eb95ca741074f54a9a6eac5e3bc51
Yanara Velásquez <yanara.velasquez@thoughtworks.com>
2024-05-30 19:38:13 -0400

    test

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "tfe_organization_name" {
   description = "Name terraform cloud organization"
   default     = "POC_Terraform_Latam"
 }
+variable "tfe_organization_name2" {
+  description = "Name terraform cloud organization"
+  default     = ""
+}
+
